e4e9440360281e59e1756009919072ae3a731073
Francis McNally <fearlessisfrank@gmail.com>
2024-03-30 20:41:54 -0400

    New file

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Value from './src/components/Value';
+import RingProgress from  './src/components/RingProgress';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <RingProgress progress={0.25}/>
+      
+      <View style={styles.values}>
+        <Value label="Steps" value="1219" />
+        <Value label="Distance" value="0.5 m" />
+        <Value label="Flights Climbed" value="12" />
+      </View>
+      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#000',
     justifyContent: 'center',
+    padding: 10,
   },
+  values: {
+    flexDirection: 'row',
+    gap:25,
+    flexWrap: 'wrap',
+    marginTop: 100,
+  },
+
 });
